81afc2ea4ca721668ac6aceec5200f34f97812d8
Alexis Gardin <alexis.gardin@univ-cotedazur.fr>
2020-02-12 09:03:36 +0100

    add basic implementation of runtime

---
+++ midi/src/main/java/fr/polytech/dsl/App.java
@@ -0,0 +1,13 @@
+package fr.polytech.dsl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
